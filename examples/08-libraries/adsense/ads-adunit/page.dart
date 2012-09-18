b39d2f9017b15b0407142586041623da6f9be837
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2012-09-18 23:28:18 +0200

    add examples available at https://google-developers.appspot.com/maps/documentation/javascript/examples/ in 'Libraries' section

---
+++ examples/08-libraries/adsense/ads-adunit/page.dart
@@ -0,0 +1,26 @@
+#import('dart:html');
+#import('../../../../gmaps.dart', prefix:'gmaps');
+#import('../../../../gmaps-adsense.dart', prefix:'gmaps_ads');
+
+void main() {
+  final mapOptions = new gmaps.MapOptions()
+    ..center = new gmaps.LatLng(36.5987, -121.8950)
+    ..zoom = 12
+    ..mapTypeId = gmaps.MapTypeId.ROADMAP
+    ;
+  final map = new gmaps.GMap(query("#map_canvas"), mapOptions);
+
+
+  final adUnitDiv = new DivElement();
+
+  // Note: replace the publisher ID noted here with your own
+  // publisher ID.
+  final adUnitOptions = new gmaps_ads.AdUnitOptions()
+    ..format = gmaps_ads.AdFormat.HALF_BANNER
+    ..position = gmaps.ControlPosition.TOP_CENTER
+    ..publisherId = 'ca-google-maps_apidocs'
+    ..map = map
+    ..$["visible"] = true
+    ;
+  final adUnit = new gmaps_ads.AdUnit(adUnitDiv, adUnitOptions);
+}
---
+++ examples/08-libraries/drawing/drawing-tools/page.dart
@@ -0,0 +1,38 @@
+#import('dart:html');
+#import('../../../../gmaps.dart', prefix:'gmaps');
+#import('../../../../gmaps-drawing.dart', prefix:'gmaps_draw');
+
+const IMAGE_URL = "https://google-developers.appspot.com/maps/documentation/javascript/examples";
+
+void main() {
+  final mapOptions = new gmaps.MapOptions()
+    ..center = new gmaps.LatLng(-34.397, 150.644)
+    ..zoom = 8
+    ..mapTypeId = gmaps.MapTypeId.ROADMAP
+    ;
+  final map = new gmaps.GMap(query("#map_canvas"), mapOptions);
+
+  final drawingManager = new gmaps_draw.DrawingManager(new gmaps_draw.DrawingManagerOptions()
+    ..drawingMode = gmaps_draw.OverlayType.MARKER
+    ..drawingControl = true
+    ..drawingControlOptions = (new gmaps_draw.DrawingControlOptions()
+      ..position = gmaps.ControlPosition.TOP_CENTER
+      ..drawingModes = [
+        gmaps_draw.OverlayType.MARKER,
+        gmaps_draw.OverlayType.CIRCLE
+      ]
+    )
+    ..markerOptions = (new gmaps.MarkerOptions()
+      ..icon = '${IMAGE_URL}/images/beachflag.png'
+    )
+    ..circleOptions = (new gmaps.CircleOptions()
+      ..fillColor = '#ffff00'
+      ..fillOpacity = 1
+      ..strokeWeight = 5
+      ..clickable = false
+      ..editable = true
+      ..zIndex = 1
+    )
+  );
+  drawingManager.setMap(map);
+}
