277466b83469783c075202765ff3140454c9d872
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2017-06-20 23:03:18 +0200

    Upgrade from comment-based generics syntax
    Fixes #45

--- lib/util/async.dart
+++ lib/util/async.dart
@@ -8,24 +8,25 @@ import 'package:js_wrapping/util/async.dart' show SubscribeStreamProvider;
 import 'package:js_wrapping/util/state.dart';
 
 const _undefined = const _Undefined();
+
 class _Undefined {
   const _Undefined();
 }
 
-SubscribeStreamProvider/*<T>*/ getStreamProviderFor/*<T>*/(dynamic instance, String eventName,
+SubscribeStreamProvider<T> getStreamProviderFor<T>(
+    dynamic instance, String eventName,
     [Function transformArguments]) {
   MapsEventListener mapsEventListener = null;
   return new SubscribeStreamProvider(subscribe: (EventSink eventSink) {
-    mapsEventListener = event.addListener(instance, eventName,
-        ([p1 = _undefined, p2 = _undefined, p3 = _undefined, p4 = _undefined,
-            p5 = _undefined]) {
-      var args = [
-        p1,
-        p2,
-        p3,
-        p4,
-        p5
-      ].takeWhile((e) => e != _undefined).toList(growable: false);
+    mapsEventListener = event.addListener(instance, eventName, (
+        [p1 = _undefined,
+        p2 = _undefined,
+        p3 = _undefined,
+        p4 = _undefined,
+        p5 = _undefined]) {
+      var args = [p1, p2, p3, p4, p5]
+          .takeWhile((e) => e != _undefined)
+          .toList(growable: false);
       var value =
           args.length == 0 ? null : args.length == 1 ? args.first : args;
       eventSink.add(transformArguments == null
@@ -37,7 +38,11 @@ SubscribeStreamProvider/*<T>*/ getStreamProviderFor/*<T>*/(dynamic instance, Str
   });
 }
 
-Stream/*<T>*/ getStream/*<T>*/(JsInterface o, Symbol s, String eventName,
-    [Function transformArguments]) => getState(o).putIfAbsent(s,
-        () => getStreamProviderFor/*<T>*/(asJsObject(o),
-        eventName, transformArguments)).stream as Stream/*<T>*/;
+Stream<T> getStream<T>(JsInterface o, Symbol s, String eventName,
+        [Function transformArguments]) =>
+    getState(o)
+        .putIfAbsent(
+            s,
+            () => getStreamProviderFor<T>(
+                asJsObject(o), eventName, transformArguments))
+        .stream as Stream<T>;
