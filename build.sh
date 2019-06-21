b9faaeaabfa7afca0d92b0e139c35489a632aff1
Alexandre Ardhuin <alexandre.ardhuin@gmail.com>
2019-06-21 09:25:48 +0200

    fix shell hints

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#! /bin/bash
+
 LIBS="adsense drawing geometry panoramio places visualization weather"
 
 rm -rf .dart_tool/build
@@ -9,8 +11,8 @@ rm -rf .dart_tool/build
 pub run build_runner build -c steps-2
 for lib in $LIBS ;
 do
-  mv lib/src/google_maps_src_${lib}.g{,g}.dart
-  sed -i 's/\.g\.dart/.gg.dart/' lib/src/google_maps_src_${lib}.dart
+  mv "lib/src/google_maps_src_${lib}.g{,g}.dart"
+  sed -i 's/\.g\.dart/.gg.dart/' "lib/src/google_maps_src_${lib}.dart"
 done
 
 rm -rf .dart_tool/build
@@ -20,6 +22,6 @@ mv lib/src/google_maps_src.g{g,}.dart
 sed -i 's/\.gg\.dart/.g.dart/' lib/src/google_maps_src.dart
 for lib in $LIBS ;
 do
-  mv lib/src/google_maps_src_${lib}.g{g,}.dart
-  sed -i 's/\.gg\.dart/.g.dart/' lib/src/google_maps_src_${lib}.dart
+  mv "lib/src/google_maps_src_${lib}.g{g,}.dart"
+  sed -i 's/\.gg\.dart/.g.dart/' "lib/src/google_maps_src_${lib}.dart"
 done
